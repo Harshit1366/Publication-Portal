90f3eca49e6d238c53e458909addbfd59ba7e73f
HARSHIT WADHWA <harsh1366@gmail.com>
2018-08-21 22:56:10 +0530

    Update EmailService.java

--- src/com/user/EmailService.java
+++ src/com/user/EmailService.java
@@ -20,19 +20,19 @@ public class EmailService {
         Properties properties = System.getProperties();
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.user", "ilikeitmyway1998@gmail.com"); // User name
-        properties.put("mail.smtp.password", "desmondmiles1998"); // password
+        properties.put("mail.smtp.user", "***************"); // User name
+        properties.put("mail.smtp.password", "**************"); // password
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("ilikeitmyway1998@gmail.com", "desmondmiles1998");
+                return new PasswordAuthentication("**************", "****************"); // Email, password
             }
         });
         try {
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("ilikeitmyway1998@gmail.com"));
+            message.setFrom(new InternetAddress("***************"));// Email
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to) ); 
             message.setSubject("NCU: Internship Facility");
             message.setText(text+id+" "+pass);
@@ -47,14 +47,14 @@ public class EmailService {
         Properties properties = System.getProperties();
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.user", "harsh1366@gmail.com"); // User name
-        properties.put("mail.smtp.password", "Harshit@31"); // password
+        properties.put("mail.smtp.user", "**************"); // User name
+        properties.put("mail.smtp.password", "***************"); // password
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("harsh1366@gmail.com", "Harshit@31");
+                return new PasswordAuthentication("*************", "***********"); // Email, password
             }
         });
         try {
@@ -63,7 +63,7 @@ public class EmailService {
             System.out.println(tfrom);
             System.out.println(to);
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("harsh1366@gmail.com"));
+            message.setFrom(new InternetAddress("******************")); // Email from
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to) ); 
             message.setSubject("NCU : PUBLICATION FACILITY "+subject);
             message.setText("Mail Sent By : "+tfrom+".\n"+text);
